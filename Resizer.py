42ae4a56f537137f2088c552021fc75ac323b1a3
vinid <chiccobia@gmail.com>
2015-05-25 19:24:48 +0200

    Create the new GUI

---
+++ Resizer.py
@@ -0,0 +1,11 @@
+__author__ = 'vinid'
+
+
+class Resizer:
+    imageArray = ""
+
+    def loadImage(self, imageArray):
+        self.imageArray = imageArray
+
+    def resize(self, N):
+        return self.imageArray
---
+++ gui.py
@@ -0,0 +1,51 @@
+"""
+This moudule creates the GUI
+
+"""
+
+from Tkinter import *
+import tkFileDialog
+import matplotlib.pyplot as plt
+from scipy import misc
+
+class GUI:
+
+    imageFile = ""
+
+    def __init__(self, master):
+        self.master = master
+        master.title("Progeto sulla DCT")
+
+        self.label = Label(master, text="Metodi Del Calcolo Scientifico")
+        self.label.pack()
+
+        self.nInput = Text(master, height=1, width=30)
+        self.nInput.pack()
+        self.nInput.insert(END, "Enter the value for N")
+
+        self.qualityInput = Text(master, height=1, width=30)
+        self.qualityInput.pack()
+        self.qualityInput.insert(END, "Enter the quality value")
+
+        self.greet_button = Button(master, text="Open Image", command=self.openImage)
+        self.greet_button.pack()
+
+        self.process_button = Button(master, text="Process Image", command=root.quit)
+        self.process_button.pack()
+
+        self.close_button = Button(master, text="Close", command=exit)
+        self.close_button.pack()
+
+    def openImage(self):
+        fileName = tkFileDialog.askopenfilename()
+        print "Percorso del file: " + fileName
+        self.imageFile = misc.imread(fileName)
+        print self.imageFile
+        plt.imshow(self.imageFile, cmap=plt.cm.gray)
+        plt.show()
+
+root = Tk()
+my_gui = GUI(root)
+root.resizable(width=FALSE, height=FALSE)
+root.geometry('{}x{}'.format(300, 300))
+root.mainloop()
